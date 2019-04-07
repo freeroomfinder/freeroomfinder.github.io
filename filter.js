0f965986c8a5cd4f50a8a077cb718860ac1aec1d
fl0under <48756476+fl0under@users.noreply.github.com>
2019-04-07 14:04:35 +0930

    add display for scheduled classes

--- filter.js
+++ filter.js
@@ -23,11 +23,11 @@ reader.readAsArrayBuffer(blob);
 function to_twelve_hour(time) {
   twelve_hour = '';
   if (time < 12) {
-    twelve_hour = time + 'AM';
+    twelve_hour = time + ' AM';
   } else if (time == 12) {
-    twelve_hour = time + 'PM';
+    twelve_hour = time + ' PM';
   } else {
-    twelve_hour = (time - 12) + 'PM';
+    twelve_hour = (time - 12) + ' PM';
   }
   return twelve_hour;
 }
@@ -37,14 +37,16 @@ window.get = function() {
   var weekday = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
   // Get current time 
   var time = new Date();
+  /*
   const day = time.getDay();
   const hour = time.getHours();
   const minute = time.getMinutes();
-  /*
-  const day = '2';
+  */
+  ///*
+  const day = '1';
   const hour = '8';
   const minute = '30';
-  */
+  //*/
 
   var declare = 'DECLARE @hour int = ' + hour +
                 ';DECLARE @minute int = ' + minute +
@@ -56,7 +58,7 @@ window.get = function() {
 
   
   var selected_rooms = 'SELECT building_name, room_number, room_name, ' + 
-    'start_hour, start_minute FROM classes ' +
+    'start_hour, start_minute, room_id FROM classes ' +
     'WHERE ' + 
     'location_type=@room_type AND ' +
     'building = @building';
@@ -70,6 +72,7 @@ window.get = function() {
     val['room_name'] = element['room_name'];
     val['start_hour'] = element['start_hour'];
     val['start_minute'] = element['start_minute'];
+    val['room_id'] = element['room_id'];
     val['occupied'] = false;
     val['time'] = 25;
     rooms.set(element['room_number'], val);
@@ -130,16 +133,95 @@ window.get = function() {
     } else {
       room_status = 'Free Until ' + to_twelve_hour(value['time']);
     }
-    var card = '<div class="card"><div class="card-room">' + key + '</div>' +
-            '<div class="card-room-type">' +
-              '<span class="text">' + value['room_name'] + '</span>' + 
-            '</div>' +
-            '<div class="card-building">' + value['building_name'] + '</div>' +
-            '<div class="main-description">' + room_status + '</div></div>';
+    var card = '<div class="card" id=' + value['room_id'] + '>' + 
+                 '<div class="card-room">' + key + '</div>' +
+                   '<div class="card-room-type">' +
+                     '<span class="text">' + value['room_name'] + '</span>' + 
+                   '</div>' +
+                   '<div class="card-building">' + value['building_name'] + '</div>' +
+                   '<div class="main-description">' + room_status + 
+                 '</div>' + 
+               '</div>';
     card_html += card;
   });
   
   console.log(rooms);
 
   document.getElementById('results').innerHTML = card_html;
+
+  function getSchedule(room_id) {
+    alasql('DECLARE @room_id int = ' + room_id);
+    var scheduled_classes= 'SELECT start_hour, start_minute, end_hour, end_minute,' +
+      'course_number, course_name FROM classes ' +
+      'WHERE ' + 
+      'room_id=@room_id AND ' +
+      'CAST(start_date AS DATETIME) <= GETDATE() AND ' +
+      'CAST(end_date AS DATETIME) >= GETDATE() AND ' +
+      'day=1';
+
+    classes_query = alasql(scheduled_classes);
+    classes_query.sort(function(a,b){return a.start_hour > b.start_hour});
+    console.log(classes_query);
+    return classes_query;
+  }
+
+  var modal = document.getElementById("modal_room");
+
+  function toggleModal() {
+    modal.classList.toggle("show-modal");
+  }
+
+  function add_classes_to_modal(classes) {
+    var content = document.getElementsByClassName("modal-content-main")[0];
+    if (classes.length == 0) {
+      content.innerHTML = "<p>No classes</p>";
+    } else {
+      var html = "";
+      html = '<div class="day-timeline">';
+      classes.forEach(function callback(_class) {
+        console.log(_class);
+        html += '<div class="day-entry">';
+        html += '<div class="course-time">';
+        html += to_twelve_hour(_class.start_hour) + ' - ' + to_twelve_hour(_class.end_hour);
+        html += '</div>';
+        html += '<div class="course">';
+        html += '<div class="day-entry-course-number">' + _class.course_number + '</div>'; 
+        html += '<div class="day-entry-course">' + _class.course_name + '</div>'; 
+        html += '</div>';
+        html += '</div>';
+      });
+      html += '</div>';
+      content.innerHTML = html;
+    }
+
+    // way to avoid this?
+    var modal_close = document.querySelector(".close");
+    modal_close.addEventListener("click", toggleModal);
+  }
+
+  var divs = document.getElementsByClassName("card");
+  for (i in divs) {
+    divs[i].onmouseover = function() {
+      this.style.cursor = 'pointer';
+    };
+    divs[i].onmouseleave = function() {
+    };
+    divs[i].onclick = function() {
+      toggleModal();
+      console.log(this.id);
+      var classes = getSchedule(this.id);
+      add_classes_to_modal(classes);
+    };
+  }
+  
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      toggleModal();
+    }
+  };
+
+  var modal_close = document.querySelector(".close");
+  modal_close.addEventListener("click", toggleModal);
+
 }
+
